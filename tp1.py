a8478df65529b006443faf3786bfb9e632295b48
Goncalo Gomes <gm.gomes@campus.fct.unl.pt>
2025-10-17 09:21:49 +0100

    Initial code

---
+++ tp1.py
@@ -0,0 +1,19 @@
+###############################################################################
+## Computer Vision 2025-2026 - NOVA FCT
+## Assignment 1
+##
+## Student 1
+## Student 2
+## 
+###############################################################################
+
+import cv2 as cv
+import numpy as np
+import os
+import glob
+
+
+# Example usage:
+if __name__ == "__main__":
+    print("Assignment 1")
+    #TODO
